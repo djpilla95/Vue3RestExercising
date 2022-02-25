b2e9c0f479c19f290cb45fd3cd85a733e8b83a74
Dominic Pilla <dominicpilla@gmail.com>
2022-02-25 14:52:22 -0700

    Properly using the Composition API for PostFeed

--- src/components/Posts/PostFeed.vue
+++ src/components/Posts/PostFeed.vue
@@ -1,28 +1,30 @@
-<script>
+<script setup>
 import Post from "./Post.vue";
+import { onMounted, inject, ref } from "vue";
 
-const API_URL = `https://jsonplaceholder.typicode.com`;
+const postData = ref(null)
+const API_URL = inject("JSON_PLACEHOLDER_API")
 
-export default {
-  data: () => ({
-    postData: "",
-  }),
-  created() {
-    this.fetchData();
-  },
-  methods: {
-    async fetchData() {
-      const url = `${API_URL}/posts`;
-      this.postData = await (await fetch(url)).json();
-    },
-  },
-  components: { Post },
-};
+onMounted(async () => {
+  if(!postData.value) {
+    await fetchPostData()
+  }
+});
+
+async function fetchPostData() {
+  const url = `${API_URL}/posts`;
+  postData.value = await (await fetch(url)).json();
+}
 </script>
 <template>
   <el-scrollbar height="40em">
     <p v-for="post in postData" :key="post.id" class="scrollbar-demo-item">
-        <Post :title="post.title" :body="post.body" :id="post.id" :userId="post.userId" />
+      <Post
+        :title="post.title"
+        :body="post.body"
+        :id="post.id"
+        :userId="post.userId"
+      />
     </p>
   </el-scrollbar>
 </template>
